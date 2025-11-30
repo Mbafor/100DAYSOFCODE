79ade92f6bb3c0667b41a5d4cac2699086d48a65
Mbafor Joshua <mbaforfoghang@gmail.com>
2025-11-30 13:52:10 +0000

    minor edits

--- Band_name_generator.py
+++ Band_name_generator.py
@@ -1,6 +1,6 @@
 # Band Name Generator
 
-print("Welcome to the Band Name Generator!")
+print("Welcome to the Band Name Generator!\n")
 city = input("What's the name of the city you grew up in? \n")
 pet = input("What's your pet's name? \n")
 band_name = city + " " + pet
